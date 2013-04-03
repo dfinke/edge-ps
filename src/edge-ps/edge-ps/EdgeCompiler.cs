0cc8ea5aac5a6a2c59e497951c46978179fe9692
Doug Finke <finked@hotmail.com>
2013-04-03 10:59:29 -0400

    Initial commit

---
+++ src/edge-ps/edge-ps/EdgeCompiler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Threading.Tasks;
+
+public class EdgeCompiler
+{
+    public Func<object, Task<object>> CompileFunc(IDictionary<string, object> parameters)
+    {
+        var script = this.NormalizeSource(parameters["source"] as string);
+
+        return (input) =>
+        {
+            var task = new Task<object>(() =>
+            {
+                PowerShell powershell = PowerShell.Create();
+                powershell.AddScript(script);
+                powershell.AddParameter("parameters", parameters);
+
+                return powershell.Invoke().Select(psobject => psobject.BaseObject).ToList();
+            });
+
+            task.Start();
+
+            return task;
+        };
+    }
+
+    private string NormalizeSource(string source)
+    {
+        if (source.EndsWith(".ps1", StringComparison.InvariantCultureIgnoreCase))
+        {
+            // Read source from file
+            source = File.ReadAllText(source);
+        }
+
+        return source;
+    }
+}
