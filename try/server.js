3bdaab3711c8eaecd6873c8d9ea7638c32a87be8
Doug Finke <finked@hotmail.com>
2013-04-10 21:18:13 -0400

    Get-Eventlog or Get-WmiObject

--- try/server.js
+++ try/server.js
@@ -2,17 +2,35 @@ var edge = require('edge')
 var express = require('express');
 var app = express();
 
-var invokePS = edge.func('ps', function() {/*
+var gwmi = edge.func('ps', function() {/*
     Get-WmiObject -ClassName $inputFromJS | 
     	ConvertTo-Html -As List
 */});
 
 app.get('/ps/wmi/:class', function(req, res) {
-	invokePS(req.params.class, function (error, result) {
+	gwmi(req.params.class, function (error, result) {
 		if (error) throw error;
-		res.send(result.toString());
+  		
+  		res.setHeader('Content-Type', 'text/html');
+  		res.end(result.join(' '));
+	});
+});
+
+var eventlog = edge.func('ps', function() {/*
+	Get-EventLog -LogName Application -Newest 100 | 
+		ConvertTo-Html -As List
+*/});
+
+app.get('/ps/eventlog', function(req, res) {
+	eventlog(null, function (error, result) {
+		if (error) throw error;
+  		
+  		res.setHeader('Content-Type', 'text/html');
+  		res.end(result.join(' '));
 	});
 });
 
 console.log('try http://localhost:8080/ps/wmi/win32_bios');
+console.log('try http://localhost:8080/ps/eventlog');
+
 app.listen(8080);
