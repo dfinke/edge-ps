96d74b4f82bdf43cce65452096bf9d7569e60f03
Doug Finke <finked@hotmail.com>
2013-04-16 18:55:33 -0400

    update

--- try/Excel/server.js
+++ try/Excel/server.js
@@ -23,11 +23,12 @@ $r = New-Object PSObject -Property @{
 $xlProcess.kill()
 
 @"
-<table border='1'>
-<tr><td>Median</td><td>$($r.Median)</td></tr>
-<tr><td>StDev</td><td>$($r.StDev)</td></tr>
-<tr><td>Var</td><td>$($r.Var)</td></tr>
-</table>
+    <h2>Calling Excel Worksheet Functions in PowerShell in a Node.js web server</h2>
+    <table border='1'>
+    <tr><td>Median</td><td>$($r.Median)</td></tr>
+    <tr><td>StDev</td><td>$($r.StDev)</td></tr>
+    <tr><td>Var</td><td>$($r.Var)</td></tr>
+    </table>
 "@
 
 */})
